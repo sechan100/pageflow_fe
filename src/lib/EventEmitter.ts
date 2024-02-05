2096c6335b97c71c0eb21fa9319f0e35baac09af
sechan100 <sechan100@gmail.com>
2024-02-05 17:07:22 +0900

    feat: 유틸 클래스 추가(EventEmitter, freeze)

---
+++ src/lib/EventEmitter.ts
@@ -0,0 +1,38 @@
+class EventEmitter {
+  // [[ 싱글턴 
+  static #instance: EventEmitter | null = null;
+  static getInstance(): EventEmitter {
+    if (EventEmitter.#instance === null) {
+      EventEmitter.#instance = new EventEmitter();
+    }
+    return EventEmitter.#instance;
+  }
+  // ]]
+  events: { [event: string]: Function[] };
+
+  private constructor() {
+    this.events = {};
+  }
+
+  // 이벤트 리스너 등록
+  on(event: string, listener: Function) {
+    if(!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(listener);
+  }
+
+  // 이벤트 발행
+  emit(event: string, ...args: any[]) {
+    if(this.events[event]) {
+      this.events[event].forEach(listener => listener(...args));
+    }
+  }
+
+  // 이벤트 리스너 제거
+  off(event: string, listenerToRemove: Function) {
+    if (this.events[event]) {
+      this.events[event] = this.events[event].filter(listener => listener !== listenerToRemove);
+    }
+  }
+}
---
+++ src/lib/freeze.ts
@@ -0,0 +1,19 @@
+function deepFreeze(object) {
+  // 먼저 객체 자신을 동결
+  Object.freeze(object);
+  
+  // 객체의 모든 프로퍼티를 순회
+  Object.keys(object).forEach((key) => {
+    const property = object[key];
+    
+    // 프로퍼티가 객체이며 아직 동결되지 않았다면
+    if (typeof property === 'object' && property !== null && !Object.isFrozen(property)) {
+      // 해당 프로퍼티를 재귀적으로 동결
+      deepFreeze(property);
+    }
+  });
+
+  return object;
+}
+
+export default deepFreeze;
