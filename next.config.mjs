06a60156471cc7fcb311e5c0c254cfcb6166a523
sechan100 <sechan100@gmail.com>
2024-02-12 21:44:04 +0900

    feat: webpack 콘솔 로그 제거 컴파일 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-  async rewrites() {
+  async rewrites() { // production에서는 사용하지 않음 / proxy 설정
     return [
       {
         source: '/:path*',
@@ -9,6 +9,22 @@ const nextConfig = {
       },
     ];
   },
+
+  webpack: (config, { dev }) => { // 웹팩 컴파일 설정
+    if (!dev) {
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true, // 콘솔 로그 제거
+            },
+          },
+        }),
+      ];
+    }
+    return config;
+  }
+
 };
 
 export default nextConfig;
